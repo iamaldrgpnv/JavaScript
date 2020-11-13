1d1008bea7948a08e1cce3d7a1f5e8113d6d1943
alex <imaldrgpnv@gmail.com>
2020-11-13 15:13:44 +0300

    Chanched HW1

--- js/home_work_1.js
+++ js/home_work_1.js
@@ -8,18 +8,16 @@ console.log(`Высота: ${h}` + "см")
 console.log(`Длина: ${l}` + "см")
 console.log(`Ширина: ${b}` + "см")
 console.log(`Площадь: ${s}` + "см2")
-
+//
 console.log("Задание 2")
-let x = "5";
-let y = "6";
-let n = x + y;
-let sum = +x + +y;
-console.log(`n: ${n}`)
-console.log(`Сумма цифр двузначного числа n: ${sum}`)
-
+let n = 31;
+let sum = (Math.floor (n / 10 + n % 10));
+console.log(`n: ${n}`);
+console.log(`Сумма цифр двузначного числа n: ${sum}`);
+//
 console.log("Задание 3")
-let num_1 = 5;
-let num_2 = 6;
+let num_1 = 16;
+let num_2 = 100;
 let changed_num_1 = (num_2 - num_1) + num_1;
 let changed_num_2 = (num_1 - num_2) + num_2;
 console.log(num_1)
