fa122dc27b48ab1caa4a52dcf80a716dec171638
alex <imaldrgpnv@gmail.com>
2020-12-02 15:00:09 +0300

    Changed_Home_Work_6

--- js/home_work_6.js
+++ js/home_work_6.js
@@ -5,6 +5,7 @@ let numsArr = [
 	[56, -9, 111, 6],
 	[11, 86, -12],
 ];
+// Вариант с удалением и добавлением элементов
 for (let i = 0; i < numsArr.length; i++) {
 	numsArr[i].forEach((a) => {
 		numsArr[i].shift(); // не понимаю, почему нельзя использовать numsArr[i].shift().push(a += 10)
@@ -12,7 +13,17 @@ for (let i = 0; i < numsArr.length; i++) {
 	})
 }
 console.log(numsArr);
-console.log("");
+// Вариант с map 
+numsArr = [
+	[3, 5, -1, 6, 0],
+	[56, -9, 111, 6],
+	[11, 86, -12],
+];
+let newArrOfOld = numsArr.map(function(arr, index, numsArr) {
+	return arr.map((number) => number += 10);
+})
+console.log(newArrOfOld)
+console.log("")
 //
 console.log("Задача №4.2")
 numsArr = [
@@ -21,13 +32,25 @@ numsArr = [
 	[11, 86, -12],
 ];
 let newArr = [];
+// Вариант с перебором массива
 for (let element of numsArr) {
 	element.forEach((a) => {
 		if (a > 0) newArr.push(a);
 	})
 }
 console.log(newArr);
-console.log("")
+// Вариант с filter
+numsArr = [
+	[3, 5, -1, 6, 0],
+	[56, -9, 111, 6],
+	[11, 86, -12],
+];
+newArr = numsArr.map(function(arr, index) {
+	let result = arr.filter((number) => number > 0);
+	return result;
+})
+console.log(newArr);
+console.log("");
 //
 console.log("Задача №5");
 let phrase = "Роза - прекрасный вИагРа цветок, который ххх дарят ВИАГРА возлюбленным хХх"
@@ -50,8 +73,8 @@ console.log(getSpam(phrase, "виагра", "ххх"));
 console.log(" ")
 //
 console.log("Задача №6");
-let more18 = (age) => { if (18 < age) return true; } // Здесь нужно использовать return, т.к. if неоднострочное действие
-let less30 = (age) => { if (age < 30) return true; }
+let more18 = (age) => (18 < age); 
+let less30 = (age) => (age < 30);
 let age = Math.floor(Math.random() * 100);
 let arr = [more18, less30];
 // function checkAge (arr, age) {
@@ -60,10 +83,8 @@ let arr = [more18, less30];
 // }
 // Потренировался использовать стрелочную функцию
 // VS Code просит переконвертировать стрелочную в именованную функцию
-// Как лучше оформлять функцию в таких случаях?
-let checkAge = (arr, number) => {
-	let result = (arr[0](number) === true && arr[1](number) === true) ? true : false;
-	return result;
-}
+// Как лучше оформлять функцию в таких случаях? (Без разницы)
+let checkAge = (arr, number) => arr[0](number) === true && arr[1](number) === true
+
 console.log(age);
 console.log(checkAge(arr, age));
