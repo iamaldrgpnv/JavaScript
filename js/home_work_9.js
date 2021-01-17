f915cd2e1208da104b76ea7128577d4416d73e90
alex <imaldrgpnv@gmail.com>
2021-01-18 01:25:29 +0300

    Генерация игрового поля с попытками

--- js/home_work_9.js
+++ js/home_work_9.js
@@ -28,17 +28,29 @@ function setPrise(object) {
 	for (let key in object) arr.push(key);
 	for (let i = 0; i < arr.length; i++) randomPresent().setAttribute("data-prise", `${arr[i]}`)
 }
+
+let attemptsJs = prompt("Сколько у вас попыток?");
+let attamptsHTML = document.querySelector(".attempts");
 let gameFieldWithPrise = document.getElementById("game-field");
 gameFieldWithPrise.addEventListener("click", open);
 function open(event) {
+	if (attemptsJs === 1) {
+		gameFieldWithPrise.removeEventListener("click", open);
+		document.querySelector(".result").innerHTML = "Better luck next time"
+	}
+	attamptsHTML.innerHTML = (`Количество попыток ${attemptsJs - 1}`)
 	let clickelem = event.target;
 	let prise = clickelem.dataset.prise
 	if (prise) {
 		clickelem.classList.add("prise");
 		clickelem.innerHTML = clickelem.dataset.prise;
+		document.querySelector(".prises").innerHTML = "Ваш приз: " + clickelem.dataset.prise;
+		document.querySelector(".result").innerHTML = "Поздравляем, вы победили"
+		gameFieldWithPrise.removeEventListener("click", open);
 	}
-	console.log(event.target)
+	console.log(event.target);
+	attemptsJs--;
 }
 // Вызов функций
-generateField(4)
-setPrise(object)
+generateField(4);
+setPrise(object);
